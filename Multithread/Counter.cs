4d35a9549041344d6adf1dba7886d925b73f24aa
liudaxiahahaah <1456447215@qq.com>
2018-07-25 12:58:28 +0800

    添加项目文件。

---
+++ Multithread/Counter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithread
+{
+    class MultithreadSample
+    {
+        public void printOneToTen()
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void printOneToX(int x)
+        {
+            for (int i = 1; i < x+1; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void printOneToX(object x)
+        {
+            for (int i = 1; i < (int)x + 1; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Multithread/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Multithread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultithreadSample m = new MultithreadSample();
+            //无参数新线程
+            Thread t = new Thread(m.printOneToTen);
+            t.Start();
+            m.printOneToTen();
+            t.Join();
+            //有参数新线程方法1
+            t = new Thread(() => m.printOneToX(50));//lambda表达式委托
+            t.Start();
+            m.printOneToX(20);
+            t.Join();
+            //有参数新线程方法2
+            t = new Thread(m.printOneToX);
+            t.Start(30);//在start中传参参数将视为object类型
+            m.printOneToX(30);
+            ReadKey();
+
+
+        }
+
+    }
+}
